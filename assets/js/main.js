cd4fc4bf54cbc766117f510f4c0e140032433277
Dilshad Shibin <49745082+dilshad-shibin@users.noreply.github.com>
2020-10-06 17:30:21 +0530

    fist add

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+
+
+    $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 2000,
+        stepSize: 120,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+      });
+    
